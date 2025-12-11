aa0ab8896fd89e9648024394feda0b3a0288af07
slothy <jacobcalvining@gmail.com>
2025-12-10 23:56:10 -0600

    adding/removing players

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,66 @@
 use std::{
-    collections::HashMap,
-    ops::DerefMut,
+    collections::{HashMap, HashSet},
     pin::Pin,
     task::{Context, Poll},
 };
 
-use futures::{FutureExt, Stream, StreamExt, future};
-use futures_util::TryStreamExt;
+use futures::StreamExt;
 use tracing::{info, instrument};
 use tracing_subscriber::{EnvFilter, fmt::format::FmtSpan};
 use zbus::{
     Connection, Proxy,
+    names::{BusName, MemberName, WellKnownName},
     proxy::SignalStream,
-    zvariant::{Array, Dict, OwnedValue, Str, Value},
+    zvariant::{OwnedValue, Str, Structure, Value},
 };
 
-const MPRIS_PLAYER: &str = "org.mpris.MediaPlayer2";
+const MPRIS_PREFIX: &str = "org.mpris.MediaPlayer2";
+const MPRIS_PATH: &str = "/org/mpris/MediaPlayer2";
+const MPRIS_PLAYER_PREFIX: &str = "org.mpris.MediaPlayer2.Player";
 
-#[derive(Debug, Default)]
+const DBUS_NAME: &str = "org.freedesktop.DBus";
+const DBUS_PATH: &str = "/org/freedesktop/DBus";
+const DBUS_PROPERTIES: &str = "org.freedesktop.DBus.Properties";
+
+#[derive(Debug)]
+enum DbusMethods {
+    ListNames,
+    GetAll,
+}
+
+impl TryFrom<DbusMethods> for MemberName<'_> {
+    type Error = zbus::names::Error;
+
+    fn try_from(value: DbusMethods) -> Result<Self, Self::Error> {
+        let s = match value {
+            DbusMethods::ListNames => "ListNames",
+            DbusMethods::GetAll => "GetAll",
+        };
+
+        Ok(MemberName::from_str_unchecked(s))
+    }
+}
+
+#[derive(Debug)]
+enum DbusSignals {
+    PropertiesChanged,
+    NameOwnerChanged,
+}
+
+impl TryFrom<DbusSignals> for MemberName<'_> {
+    // so that things that require TryFrom<MemberName> work for this type
+    type Error = zbus::names::Error;
+    fn try_from(value: DbusSignals) -> Result<Self, Self::Error> {
+        let s = match value {
+            DbusSignals::PropertiesChanged => "PropertiesChanged",
+            DbusSignals::NameOwnerChanged => "NameOwnerChanged",
+        };
+
+        Ok(MemberName::from_str_unchecked(s))
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
 enum PlaybackStatus {
     #[default]
     Stopped,
@@ -40,7 +83,7 @@ impl<'a> TryFrom<&Value<'a>> for PlaybackStatus {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone, Copy)]
 enum LoopStatus {
     #[default]
     None,
@@ -66,16 +109,17 @@ impl<'a> TryFrom<&Value<'a>> for LoopStatus {
 #[derive(Debug)]
 #[allow(dead_code)]
 struct Metadata {
-    art_url: String,
-    length: u64,
+    art_url: Option<String>,
+    length: Option<u64>,
     trackid: String,
     album: Option<String>,
-    artist: Vec<String>,
+    artists: Vec<String>,
     title: String,
     url: String,
     track_number: Option<i32>,
     disc_number: Option<i32>,
     auto_rating: Option<f64>,
+    album_artists: Option<Vec<String>>,
 }
 
 impl<'a> TryFrom<&Value<'a>> for Metadata {
@@ -107,37 +151,34 @@ impl<'a> TryFrom<&Value<'a>> for Metadata {
 impl<'a> From<HashMap<String, Value<'a>>> for Metadata {
     #[instrument]
     fn from(value: HashMap<String, Value<'a>>) -> Self {
-        let art_url: String = value.get("mpris:artUrl").unwrap().try_into().unwrap();
-        let length: u64 = match value.get("mpris:length") {
-            Some(f) => match f {
-                Value::I64(s) => *s as u64,
-                Value::U64(s) => *s,
-                _ => unimplemented!(),
-            },
-            None => {
-                unimplemented!()
-            }
-        };
-        let trackid: String = match value.get("mpris:trackid") {
-            Some(f) => match f {
-                Value::ObjectPath(s) => s.to_string(),
-                Value::Str(s) => s.to_string(),
-                _ => unimplemented!(),
-            },
-            None => {
-                unimplemented!()
-            }
-        };
-
-        let album: Option<String> = match value.get("xesam:album") {
-            Some(f) => match f {
+        let art_url: Option<String> = match value.get("mpris:artUrl") {
+            Some(url) => match url {
                 Value::Str(s) => Some(s.to_string()),
                 _ => unimplemented!(),
             },
-
             None => None,
         };
-        let artist: Vec<String> = value
+        // optional because players like browsers can not include the length when we request its
+        // metadata but might give us the length later
+        let length = match value.get("mpris:length") {
+            Some(Value::I64(s)) => Some(*s as u64),
+            Some(Value::U64(s)) => Some(*s),
+            None => None,
+            _ => unimplemented!(),
+        };
+        let trackid: String = match value.get("mpris:trackid") {
+            Some(Value::ObjectPath(s)) => s.to_string(),
+            Some(Value::Str(s)) => s.to_string(),
+            _ => unimplemented!(),
+        };
+
+        let album: Option<String> = match value.get("xesam:album") {
+            Some(Value::Str(s)) => Some(s.to_string()),
+            None => None,
+
+            _ => unimplemented!(),
+        };
+        let artists: Vec<String> = value
             .get("xesam:artist")
             .unwrap()
             .try_clone()
@@ -148,47 +189,57 @@ impl<'a> From<HashMap<String, Value<'a>>> for Metadata {
         let url: String = value.get("xesam:url").unwrap().try_into().unwrap();
 
         // optional (basically only spotify implements this)
-        value.get("xesam:albumArtist");
-        value.get("xesam:autoRating");
-        value.get("xesam:discNumber");
-        value.get("xesam:trackNumber");
+        let album_artist = match value.get("xesam:albumArtist") {
+            Some(Value::Array(s)) => Some(
+                s.iter()
+                    .filter_map(|f| {
+                        if let Value::Str(s) = f {
+                            Some(s.to_string())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect(),
+            ),
+            s => {
+                info!(?s);
+                None
+            }
+        };
 
         let track_number = {
             match value.get("xesam:trackNumber") {
-                Some(val) => match val {
-                    Value::I32(v) => Some(*v),
-                    _ => unreachable!(),
-                },
+                Some(Value::I32(s)) => Some(*s),
                 None => None,
+                _ => unreachable!(),
             }
         };
 
         let disc_number = {
             match value.get("xesam:discNumber") {
-                Some(val) => match val {
-                    Value::I32(v) => Some(*v),
-                    _ => unreachable!(),
-                },
+                Some(Value::I32(s)) => Some(*s),
                 None => None,
+
+                _ => unreachable!(),
             }
         };
 
         let auto_rating = {
             match value.get("xesam:autoRating") {
-                Some(val) => match val {
-                    Value::F64(v) => Some(*v),
-                    _ => unreachable!(),
-                },
+                Some(Value::F64(v)) => Some(*v),
                 None => None,
+
+                _ => unreachable!(),
             }
         };
 
         Self {
+            album_artists: album_artist,
             art_url,
             length,
-            trackid: trackid.to_string(),
+            trackid,
             album,
-            artist,
+            artists,
             title,
             url,
             track_number,
@@ -263,7 +314,52 @@ impl<'a> From<HashMap<&str, Value<'a>>> for PlayerCapabilities {
     }
 }
 
-async fn try_poll_signal<'a>(stream: &mut SignalStream<'a>) {}
+#[derive(Debug)]
+struct Player<'a> {
+    capabilities: PlayerCapabilities,
+    stream: SignalStream<'a>,
+}
+
+impl<'a> Player<'a> {
+    #[instrument]
+    async fn new(conn: &Connection, name: String) -> Self {
+        println!("name {name:?}");
+        let properties = conn
+            .call_method(
+                Some(name.clone()),
+                MPRIS_PATH,
+                Some(DBUS_PROPERTIES),
+                DbusMethods::GetAll,
+                &(MPRIS_PLAYER_PREFIX),
+            )
+            .await
+            .unwrap();
+
+        let body = properties.body();
+        let properties: PlayerCapabilities =
+            body.deserialize::<HashMap<&str, Value>>().unwrap().into();
+
+        let cloned = name.clone();
+        let proxy = Proxy::new(
+            conn,
+            BusName::WellKnown(WellKnownName::from_str_unchecked(&cloned)),
+            MPRIS_PATH,
+            DBUS_PROPERTIES,
+        )
+        .await
+        .unwrap();
+
+        let stream = proxy
+            .receive_signal(DbusSignals::PropertiesChanged)
+            .await
+            .unwrap();
+
+        Self {
+            capabilities: properties,
+            stream,
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -284,23 +380,23 @@ async fn main() {
     let conn = Connection::session().await.unwrap();
     let msg = conn
         .call_method(
-            Some("org.freedesktop.DBus"),
-            "/org/freedesktop/DBus",
-            Some("org.freedesktop.DBus"),
-            "ListNames",
+            Some(DBUS_NAME),
+            DBUS_PATH,
+            Some(DBUS_NAME),
+            DbusMethods::ListNames,
             &(),
         )
         .await
         .unwrap();
 
     let body = msg.body();
-    let data: Vec<String> = body
+    let data: Vec<Box<_>> = body
         .deserialize::<Vec<&str>>()
         .unwrap()
         .iter()
         .filter_map(|f| {
-            if f.starts_with(MPRIS_PLAYER) {
-                Some(f.to_string())
+            if f.starts_with(MPRIS_PREFIX) {
+                Some(Box::new(*f))
             } else {
                 None
             }
@@ -308,70 +404,154 @@ async fn main() {
         .collect();
 
     println!("{data:?}");
-    let mut proxies: Vec<(String, SignalStream)> = vec![];
 
-    for player in data.iter() {
-        // let properties = conn
-        //     .call_method(
-        //         Some(player.clone()),
-        //         "/org/mpris/MediaPlayer2",
-        //         Some("org.freedesktop.DBus.Properties"),
-        //         "GetAll",
-        //         &("org.mpris.MediaPlayer2.Player"),
-        //     )
-        //     .await
-        //     .unwrap();
+    let mut players: HashMap<&str, Player> = HashMap::new();
 
-        let proxy = Proxy::new(
-            &conn,
-            player.clone(),
-            "/org/mpris/MediaPlayer2",
-            "org.freedesktop.DBus.Properties",
-        )
+    for player_name in &data {
+        let player = Player::new(&conn, player_name.to_string()).await;
+        players.insert(player_name, player);
+    }
+
+    let name_changed = Proxy::new(&conn, DBUS_NAME, DBUS_PATH, DBUS_NAME)
         .await
         .unwrap();
 
-        let stream = proxy.receive_signal("PropertiesChanged").await.unwrap();
-
-        proxies.push((player.clone(), stream));
-    }
+    let mut stream = name_changed
+        .receive_signal(DbusSignals::NameOwnerChanged)
+        .await
+        .unwrap();
 
     let waker = futures::task::noop_waker();
     let mut cx = Context::from_waker(&waker);
-    loop {
-        for (name, stream) in proxies.iter_mut() {
-            match Pin::new(stream).poll_next_unpin(&mut cx) {
-                Poll::Ready(Some(msg)) => {
-                    let (iface, changed, invalidated): (
-                        String,
-                        HashMap<String, OwnedValue>,
-                        Vec<String>,
-                    ) = msg.body().deserialize().unwrap();
-                    if let Some(status) = changed.get("PlaybackStatus") {
-                        let val = &**status;
-                        let status: PlaybackStatus = val.try_into().unwrap();
+    let mut pollable_stream = Pin::new(&mut stream);
+    while let Some(player) = players.iter_mut().next() {
+        match Pin::new(&mut player.1.stream).poll_next_unpin(&mut cx) {
+            Poll::Ready(Some(msg)) => {
+                let body = msg.body();
+                // returns interface, changed (vec), invalidated (vec), invalidated seems to always
+                // be empty
+                let s: Structure = body.deserialize().unwrap();
+
+                let iface: Str = s.fields()[0].clone().try_into().unwrap();
+                let changed: HashMap<String, OwnedValue> =
+                    s.fields()[1].clone().try_into().unwrap();
+
+                println!("iface {iface}; changed {changed:?}");
+                if let Some(status) = changed.get("PlaybackStatus") {
+                    let val = &**status;
+                    player.1.capabilities.playback_status = val.try_into().unwrap();
+                }
+                if let Some(status) = changed.get("Metadata") {
+                    let val = &**status;
+                    if let Value::Dict(dict) = val {
+                        let map: HashMap<String, Value> =
+                            dict.try_clone().unwrap().try_into().unwrap();
+                        let metadata: Metadata = map.into();
+                        println!("{metadata:?}");
                     }
-                    if let Some(status) = changed.get("Metadata") {
-                        let val = &**status;
-                        if let Value::Dict(dict) = val {
-                            let map: HashMap<String, Value> =
-                                dict.try_clone().unwrap().try_into().unwrap();
-                            let metadata: Metadata = map.try_into().unwrap();
-                            println!("{metadata:?}");
+                }
+                if let Some(status) = changed.get("CanGoPrevious") {
+                    player.1.capabilities.can_previous = status.try_into().unwrap();
+                }
+            }
+            Poll::Ready(None) => {
+                // stream ended
+            }
+            Poll::Pending => {
+                // no message available (non-blocking)
+            }
+        }
+        if let Poll::Ready(Some(msg)) = pollable_stream.poll_next_unpin(&mut cx) {
+            let (name, old_owner, new_owner): (String, String, String) =
+                msg.body().deserialize().unwrap();
+
+            if name.starts_with(MPRIS_PREFIX) {
+                println!("name {name}; old_owner {old_owner}; new_owner {new_owner}");
+
+                match (old_owner.is_empty(), new_owner.is_empty()) {
+                    // added player
+                    // needs to call ListNames to convert the name (unique name) to well known name
+                    (true, false) => {
+                        let msg = conn
+                            .call_method(
+                                Some(DBUS_NAME),
+                                DBUS_PATH,
+                                Some(DBUS_NAME),
+                                DbusMethods::ListNames,
+                                &(),
+                            )
+                            .await
+                            .unwrap();
+
+                        let body = msg.body();
+                        let data: HashSet<&str> = body
+                            .deserialize::<Vec<&str>>()
+                            .unwrap()
+                            .iter()
+                            .filter_map(|f| {
+                                if f.starts_with(MPRIS_PREFIX) {
+                                    Some(*f)
+                                } else {
+                                    None
+                                }
+                            })
+                            .collect();
+
+                        let mut idx = "";
+                        for player_name in players.keys() {
+                            if data.contains(player_name) {
+                                idx = player_name;
+                                break;
+                            }
                         }
+
+                        if idx.is_empty() {
+                            unimplemented!()
+                        }
+                        let p = Player::new(&conn, idx.to_string()).await;
+                        println!("added player {p:?}");
+                        players.insert(idx, p);
                     }
-                    if let Some(status) = changed.get("CanGoPrevious") {
-                        let val: bool = status.try_into().unwrap();
+                    // removed player
+                    (false, true) => {
+                        let msg = conn
+                            .call_method(
+                                Some(DBUS_NAME),
+                                DBUS_PATH,
+                                Some(DBUS_NAME),
+                                DbusMethods::ListNames,
+                                &(),
+                            )
+                            .await
+                            .unwrap();
+
+                        let body = msg.body();
+                        let data: HashSet<&str> = body
+                            .deserialize::<Vec<&str>>()
+                            .unwrap()
+                            .iter()
+                            .filter_map(|f| {
+                                if f.starts_with(MPRIS_PREFIX) {
+                                    Some(*f)
+                                } else {
+                                    None
+                                }
+                            })
+                            .collect();
+
+                        let mut idx = "";
+                        for player_name in players.keys() {
+                            if data.contains(player_name) {
+                                idx = *player_name;
+                            }
+                        }
+
+                        println!("removing player {idx:?}");
+                        _ = players.remove(idx);
                     }
 
-                    println!("name {name:?} {iface} {changed:?} {invalidated:?}");
-                }
-                Poll::Ready(None) => {
-                    // stream ended
-                }
-                Poll::Pending => {
-                    // no message available (non-blocking)
-                }
+                    _ => {}
+                };
             }
         }
     }
